f4d9cd99a7dfa260ba6169536261047b0ffab62f
Juryynan <juho_ryynanen@hotmail.com>
2024-02-06 03:10:56 +0200

    Initial commit

--- ATM.c
+++ ATM.c
@@ -1,106 +1,272 @@
-#include <stdio.h>
-#include <ctype.h>
+/*  ATM program
+    
+    Written by Juho Ryynänen
 
-int balance = 1000;
-//function for displaying ATM
-int display_menu();
-void add_balance(); 
-void view_balance();
-void withdraw();
-void print_receipt();
+    With this small ATM program user can view account information, add money, withdraw money and print recipe
+
+    Maybe to-do: 
+    1. Update data to file when deposit and withdrawal are completed.
+    2. File for logging transactions.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+
+#define MAX_ACCOUNTS 100
+
+typedef struct {
+    int accountId;
+    char creditCard[30];
+    int pin;
+    char name[30];
+    double balance;
+} Account;
+
+Account Accounts[MAX_ACCOUNTS];
+
+bool readAccounts();
+void deposit(Account *customer); 
+void viewBalance(Account customer);
+void withdraw(Account *customer);
+void printReceipt(Account customer);
+void runATM();
+void displayMenu();
+bool userValid(char creaditCard[30], int pin);
+
+
+Account userSelection(char creaditCard[30], int pin);
 
 
 int main (void){
 
+    readAccounts();
+    runATM();
+   
+    return 0;
+
+}
+
+
+void runATM(){
+     Account customer;
+
+    //printf("%s %d", Accounts[0].creditCard,  Accounts[0].pin);
     enum operation {view = 1, add, withdrawal, receipt, stop};
 
-    int flag = 1;
-    int choice;
-    
-    do {
+    int flag = 0,  logged = 1,  tries = 3, choice, pin;
+    char card[30];
+  
 
-        choice = display_menu();
-       
-    
-        if(choice == view){
-            view_balance();
-        } else if (choice == add){
-            add_balance();
-        } else if (choice == withdrawal){
-           withdraw();
-        } else if (choice == receipt){
-            print_receipt();
-            flag = 0;
-        } else if (choice == stop){
-           flag = 0;
+    while(logged == 1){
+        
+        printf("-ATM-\n");
+        printf("\nCARD:");
+        scanf("%s", card);
+        
+        if(strcmp("exit", card) == 0){
+            printf("Goodbye!..");
+            exit(0);
         }
 
-    } while(flag);
+        printf("\nPIN: ");
+        scanf("%d", &pin);
+       
+        if(userValid(card, pin)){
+            
+            customer = userSelection(card, pin);
+            logged = 0;
+            printf("Welcome! %s\n", customer.name);
+    
+        } else if (!userValid(card, pin)) {
+            
+            printf("\nDeclined, take card...\n");
+            tries++;
+        } 
+    }
+
+    do {
+  
+        displayMenu();
+        printf("\n> ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case view:
+           viewBalance(customer);
+        break;
+
+        case add:
+            deposit(&customer);
+        break;
+
+        case withdrawal:
+            withdraw(&customer);
+        break;
+
+        case receipt:
+             printReceipt(customer);
+        break;
+        
+        case stop:
+            exit(0);
+            break;
+        }
+   
+    } while(true);
 
     printf("\nHave a nice day..");
-
 }
 
 //Function that displays the menu
 
-int display_menu(void){
+void displayMenu(void){
     
-    int choice;
-    printf("ATM\n");
-    printf("1. View balance\n");
-    printf("2. Deposit\n");
-    printf("3. Withdraw\n");
-    printf("4. Receipt\n");
-    printf("5. EXIT\n");
+    const char *menu[] = {
+        "View balance",
+        "Deposit",
+        "Withdraw",
+        "Receipt",
+        "Exit"
+    };
 
-    scanf("%d", &choice);
-    
-    return choice;
-    
+    printf("------ATM-------\n");
+
+    for(int i = 0; i < sizeof(menu) / sizeof(menu[0]); i++){
+        printf("%d. %s\n", i+1, menu[i]);
+    }
+
 }
 
-//
 
-void view_balance(){
-     printf("\nAccount:");
-     printf("\nAccount balance: %d €\n", balance);
+void viewBalance(Account a){
+     printf ("\nAccount information");
+     printf("\n%s", a.creditCard);
+     printf("\nAvailable Balance: %lf €\n", a.balance);
+     printf("\nTransactions");
+     printf("\n");
+   
 }       
 
-//Function checks accounts balance
 
-void add_balance(){
+void deposit(Account *a){
+    
     int amount = 0;
-    printf("\nGive amount: ");
+   
+    printf("\nSUM: ");
     scanf("%d", &amount);
+   
     if(amount > 0 ){
-        balance += amount;
+        a->balance += amount;
     }
+   
     printf("\n");
+ 
 }
 
-//Function for withdrawal
 
-void withdraw(){
+void withdraw(Account *user){
+   
     int amount = 0;
     int choice = 1;
     
     while(choice){
 
-        view_balance();
-        printf("\nGive amount: ");
+        viewBalance(*user);
+        printf("\nSUM: ");
         scanf("%d", &amount);
-        if((balance >= amount)){
-            balance -= amount;
-        } else if (amount > balance) {
-            printf("\namount too large");
-        } else if (balance = 0){
-            printf("\nbalance is zero");
+        
+        if((user->balance >= amount)){
+            user->balance -= amount;
+            choice = 0;
+        } 
+        if (amount > user->balance) {
+            printf("\nError in withdrawal");
+        } 
+        if (user->balance == 0){
+            printf("\nError in withdrawal");
         }
     }
+
 }
 
-void print_receipt(){
+void printReceipt(Account a){
+    
+    // Time reading
+    time_t now = time(&now);
+    struct tm *ptm = gmtime(&now);
+    
     printf("\nReceipt\n");
-    printf("Account balance: %d", balance);
+    printf("Time: %s", asctime(ptm));
+    printf("Account balance: %d\n", a.balance);
+    printf("Card holder: %s\n", a.name);
+
     printf("\n");
+}
+
+
+bool readAccounts(){
+
+    FILE *file = fopen("accounts.txt", "r");
+
+    int errnum;
+    
+    if (file == NULL)
+    {
+        fprintf(stderr, "\nOUT OF ORDER! %s\n", strerror( errnum ));
+        return false;
+    }
+    
+    int read = 0;
+    int records = 0;
+
+    do {
+     
+      read = fscanf(file, "%d,%29[^,],%d,%29[^,],%lf\n",
+                      &Accounts[records].accountId,
+                      Accounts[records].creditCard,
+                      &Accounts[records].pin,
+                      Accounts[records].name,
+                      &Accounts[records].balance);
+
+        if(read == 5) {
+            records++; 
+        } else if (read != 5 && !feof(file)){
+            printf("Error in file format.");
+            return 1;
+        }
+
+    } while (!feof(file));
+   
+     fclose(file);
+    
+}
+
+bool userValid(char creditCard[30], int pin) {
+    for (int i = 0; i < MAX_ACCOUNTS; i++) {
+        int compare = strcmp(Accounts[i].creditCard, creditCard);
+        if (compare == 0 && Accounts[i].pin == pin) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Account userSelection(char card[30], int pin){
+
+    for(int i = 0; i < MAX_ACCOUNTS; i++){
+        int compare = strcmp(Accounts[i].creditCard, card);
+        if (compare == 0 && Accounts[i].pin == pin) {
+            return Accounts[i];
+        } else {
+            printf("\nError in user selection.");
+        }
+    }
+
 }
